539e36b579275448ff71ddbb293ec40e3539e871
TheCraztWolf <tcrazywolf63@gmail.com>
2024-12-12 21:43:40 +0400

    Онлайн загрузка слов

--- AntiGames/Services/DisallowWordsConfiguration.cs
+++ AntiGames/Services/DisallowWordsConfiguration.cs
@@ -1,6 +1,53 @@
-﻿namespace AntiGames.Services;
+﻿using System.Text.Json;
 
-public class DisallowWordsConfiguration(IConfiguration configuration)
+namespace AntiGames.Services;
+
+public class DisallowWordsConfiguration
 {
-    public IList<string> DisallowWords { get; } = configuration.GetSection("DisallowWords").Get<IList<string>>() ?? new List<string>();
+    public IList<string> DisallowWords { get; private set; }
+    private readonly IConfiguration _configuration;
+
+    private string _url =
+        "https://raw.githubusercontent.com/TheCrazyWolf/AntiGames/refs/heads/master/AntiGames/appsettings.json";
+
+    public DisallowWordsConfiguration(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        DisallowWords = GetOnlineDisallowWords() ?? GetDefaultDisallowWords();
+    }
+
+    private IList<string>? GetOnlineDisallowWords()
+    {
+        using HttpClient client = new HttpClient();
+        try
+        {
+            return TryDeserializeDisallowWordsOrGetNull(client.GetStringAsync(_url).GetAwaiter().GetResult());
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    private IList<string> GetDefaultDisallowWords()
+    {
+        return _configuration.GetSection("DisallowWords").Get<IList<string>>() ?? new List<string>();
+    }
+
+    private IList<string>? TryDeserializeDisallowWordsOrGetNull(string jsonContent)
+    {
+        try
+        {
+            return JsonSerializer.Deserialize<ResultDissallow>(jsonContent)?.DisallowWords;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    internal class ResultDissallow
+    {
+        public List<string> DisallowWords { get; set; } = default!;
+    }
 }

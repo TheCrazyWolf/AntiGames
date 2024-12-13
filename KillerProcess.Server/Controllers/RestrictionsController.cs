b0c7c8dd89b9d805bcf920ea84990b6063f42e64
TheCraztWolf <tcrazywolf63@gmail.com>
2024-12-13 22:30:02 +0400

    Реализация сервера

---
+++ KillerProcess.Server/Controllers/RestrictionsController.cs
@@ -0,0 +1,38 @@
+using KillerProcess.Shared.Configs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KillerProcess.Server.Controllers;
+
+[ApiController]
+[Route("restrictions")]
+public class RestrictionsController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<RestrictionsController> _logger;
+
+    public RestrictionsController(ILogger<RestrictionsController> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    [HttpGet("GetRestrictions")]
+    public ConfigurationResponse Get()
+    {
+        var response = new ConfigurationResponse
+        {
+            DisallowedWords = _configuration.GetSection("ConfigWatcher:DisallowWords").Get<List<string>>() ?? new List<string>(),
+            DisallowedProcesses = _configuration.GetSection("ConfigWatcher:DisallowProcesses").Get<List<string>>() ?? new List<string>(),
+            RestrictedUsers = _configuration.GetSection("ConfigWatcher:RestrictedNTUsers").Get<List<string>>() ?? new List<string>(),
+        };
+
+        return response;
+    }
+    
+    [HttpGet("GetTestKey")]
+    public IActionResult GetTestKey()
+    {
+        var testValue = _configuration["TestKey"];
+        return Ok(new { TestKey = testValue });
+    }
+}
---
+++ KillerProcess.Shared/Configs/ConfigurationResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace KillerProcess.Shared.Configs;
+
+public class ConfigurationResponse
+{
+    public IList<string> DisallowedWords { get; set; } = new List<string>();
+    public IList<string> DisallowedProcesses { get; set; } = new List<string>();
+    public IList<string> RestrictedUsers { get; set; } = new List<string>();
+}

36cb9eaea3f7614046fca18797d373e5a7b78fb1
kulagin <alex_kulagin@samgk.ru>
2024-12-17 11:39:48 +0400

    Вынес настройку порта в конфиг файла

--- KillerProcess.Server/Program.cs
+++ KillerProcess.Server/Program.cs
@@ -1,9 +1,14 @@
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+builder.WebHost.ConfigureKestrel((httpClient, options) =>
+{
+    options.Listen(IPAddress.Any, httpClient.Configuration.GetValue<int?>("PortServer") ?? 5000);
+});
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
